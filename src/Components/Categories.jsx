8f31a2749e0713f0ee340b5013195e54ac681918
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2023-11-06 14:50:55 +0300

    Добавил useState в компонент Categories

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,16 +1,35 @@
-function Categories() {
-    return (
-      <div class="categories">
-        <ul>
-          <li class="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
-        </ul>
-      </div>
-    )
-  }
+import React from 'react';
 
-  export default Categories;
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategories = (i) => {
+    setActiveIndex(i);
+  };
+  return (
+    <div className="categories">
+      <ul>
+        <li onClick={() => onClickCategories(0)} className={activeIndex === 0 ? 'active' : ''}>
+          Все
+        </li>
+        <li onClick={() => onClickCategories(1)} className={activeIndex === 1 ? 'active' : ''}>
+          Мясные
+        </li>
+        <li onClick={() => onClickCategories(2)} className={activeIndex === 2 ? 'active' : ''}>
+          Вегетарианская
+        </li>
+        <li onClick={() => onClickCategories(3)} className={activeIndex === 3 ? 'active' : ''}>
+          Гриль
+        </li>
+        <li onClick={() => onClickCategories(4)} className={activeIndex === 4 ? 'active' : ''}>
+          Острые
+        </li>
+        <li onClick={() => onClickCategories(5)} className={activeIndex === 5 ? 'active' : ''}>
+          Закрытые
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
